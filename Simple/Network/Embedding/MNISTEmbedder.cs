e09bee88070b79f4f64f7c3d81934a4525ad6da9
Barion <ametrin.studios@gmail.com>
2024-04-23 11:28:18 +0200

    stuff

--- Simple/Network/Embedding/MNISTEmbedder.cs
+++ Simple/Network/Embedding/MNISTEmbedder.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Immutable;
-using System.Diagnostics;
 
 namespace Simple.Network.Embedding;
 
--- Simple/Program.cs
+++ Simple/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Simple;
 using Simple.Network;
-using Simple.Network.Activation;
 using Simple.Network.Embedding;
 using Simple.Network.Layer;
 using Simple.Serialization.Activation;
@@ -14,49 +13,45 @@ ActivationMethodSerializer.RegisterDefaults();
 var mnistDataSource = new MNISTDataSource(new(@"C:\Users\Nation\OneDrive - Schulen Stadt Schwäbisch Gmünd\Data\MNIST_ORG.zip"));
 var images = new ImageDataSource(new(@"C:\Users\Nation\OneDrive\Digits"));
 
-//Console.WriteLine(images.DataSet[0].DumpImage());
-
-//return;
 var config = new SimpleTrainingConfig<Number[], int>() {
     TrainingSet = mnistDataSource.TrainingSet,
     TestSet = mnistDataSource.TestingSet,
     LearnRate = 0.7,
     LearnRateMultiplier = 0.994,
-    TrainingBatchSize = 256,
+    TrainingBatchSize = 512,
     TestBatchSize = 256,
     Iterations = 128*4,
-    InputNoise = new RandomInputNoise(0.2f, new Random(420)),
+    InputNoise = new RandomInputNoise(0.2f/*, new Random(420) */),
     DumpEvaluationAfterIterations = 48,
     CostFunction = CrossEntropyCost.Instance,
     OutputResolver = new MNISTOutputResolver(),
-    RandomSource = new Random(42),
+    //RandomSource = new Random(42),
 };
 
 var serializer = new NetworkSerializer<Number[], int, RecordingLayer>(new FileInfo(@"C:\Users\Nation\Downloads\digits.nnw"));
-var setupRandom = new Random(69);
+var setupRandom = Random.Shared; //new Random(69);
 
-//var network = serializer.Load<RecordingNetwork<Number[], int>>(MNISTEmbedder.Instance).ReduceOrThrow();
+var network = serializer.Load<RecordingNetwork<Number[], int>>(MNISTEmbedder.Instance).ReduceOrThrow();
 
-var network = NetworkBuilder.Recorded<Number[], int>(784)
-    .SetDefaultActivationMethod(LeakyReLUActivation.Instance)
-    .SetEmbedder(MNISTEmbedder.Instance)
-    //.AddRandomizedLayer(128, setupRandom)
-    .AddRandomizedLayer(128, setupRandom)
-    .AddLayer(10, builder => builder.InitializeRandom(setupRandom).SetActivationMethod(SoftmaxActivation.Instance))
-    .Build();
+// var network = NetworkBuilder.Recorded<Number[], int>(784)
+//     .SetDefaultActivationMethod(LeakyReLUActivation.Instance)
+//     .SetEmbedder(MNISTEmbedder.Instance)
+//     .AddRandomizedLayer(128, setupRandom)
+//     .AddLayer(10, builder => builder.InitializeRandom(setupRandom).SetActivationMethod(SoftmaxActivation.Instance))
+//     .Build();
 
 var trainer = new NetworkTrainer<Number[], int>(config, network);
 
-var trainingResults = trainer.Train();
-Console.WriteLine(trainingResults.DumpShort());
+//var trainingResults = trainer.Train();
+//Console.WriteLine(trainingResults.DumpShort());
 
-serializer.Save(network);
+//serializer.Save(network);
 
 //Console.WriteLine(trainer.Evaluate().DumpShort());
 
 foreach(var image in images.DataSet) {
-    Console.WriteLine();
-    Console.WriteLine(image.DumpImage());
+    //Console.WriteLine();
+    //Console.WriteLine(image.DumpImage());
     Console.Write($"Prediction: {network.Process(image.Image)} ");
-    Console.WriteLine($"\t\t Actual: {image.Digit}");
+    Console.WriteLine($"\t Actual: {image.Digit}");
 }
