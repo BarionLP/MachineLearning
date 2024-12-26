8511b11d1dd05746dd1dfe6c31f76ef240f8b660
Barion <ametrin.studios@gmail.com>
2024-12-26 21:32:31 +0100

    slm3 und c4dataset

---
+++ MachineLearning.Samples/Language/C4DataSet.cs
@@ -0,0 +1,132 @@
+using System.IO.Compression;
+using System.Text.Json;
+using MachineLearning.Data;
+
+namespace MachineLearning.Samples.Language;
+
+public sealed class C4DataSet(ITokenizer<string> tokenizer, int contextSize, int initalFile = 0) : ITrainingSet, IDisposable
+{
+    public int BatchCount { get; } = int.MaxValue;
+    public required int BatchSize { get; init; }
+    public int CurrentFile => nextFile-1;
+    public int CurrentLinesRead => currentFile?.LinesRead ?? 0;
+    private int nextFile = initalFile;
+    private C4FileReader? currentFile;
+
+    private readonly ITokenizer<string> tokenizer = tokenizer;
+    private readonly int contextSize = contextSize;
+    private Task<FileInfo> downloadTask = Download(initalFile);
+
+    public IEnumerable<Batch> GetBatches()
+    {
+        while (true)
+        {
+            yield return new Batch(GetTrainingData());
+        }
+    }
+
+    public IEnumerable<TrainingData> GetTrainingData()
+        => GetTokenizedLines().Take(BatchSize).ExpandPerToken(tokenizer.TokenizeSingle("\0"), contextSize).ToTrainingData(tokenizer.TokenCount);
+
+    public IEnumerable<int[]> GetTokenizedLines()
+        => GetLines().TokenizeSkipInvalid(tokenizer);
+
+    public IEnumerable<string> GetLines()
+    {
+        while (true)
+        {
+            string? line = null;
+            try
+            {
+                line = NextLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                yield break;
+            }
+            yield return line;
+        }
+    }
+
+    private string NextLine()
+    {
+    lable:
+        if (currentFile?.ReadLine() is string line)
+        {
+            return line;
+        }
+
+        currentFile?.Dispose();
+        if (!downloadTask.IsCompleted)
+        {
+            Console.WriteLine("Waiting for download to complete...");
+        }
+        if (downloadTask.IsFaulted)
+        {
+            Console.WriteLine($"Download failed: {downloadTask.Exception?.Message}");
+        }
+        Task.WaitAll(downloadTask);
+        currentFile = new C4FileReader(downloadTask.Result);
+        nextFile++;
+        downloadTask = Download(nextFile);
+        Console.WriteLine("Downloading next file...");
+        goto lable;
+    }
+
+    public void Dispose()
+    {
+        currentFile?.Dispose();
+    }
+
+    public static async Task<FileInfo> Download(int fileIndex)
+    {
+        var file = AssetManager.GetDataFile($"c4-train_noblock/{fileIndex:D5}-of-01024.json.gz");
+        if (!file.Exists)
+        {
+            using var client = new HttpClient();
+            using var stream = await client.GetStreamAsync($"https://huggingface.co/datasets/allenai/c4/resolve/main/en.noblocklist/c4-train.{fileIndex:D5}-of-01024.json.gz");
+            using var fileStream = file.Create();
+            await stream.CopyToAsync(fileStream);
+        }
+        return file;
+    }
+
+    private sealed class C4FileReader : IDisposable
+    {
+        private readonly Stream stream;
+        private readonly GZipStream gzStream;
+        private readonly StreamReader reader;
+        public int LinesRead { get; private set; }
+
+        public C4FileReader(FileInfo fileInfo)
+        {
+            stream = fileInfo.OpenRead();
+            gzStream = new GZipStream(stream, CompressionMode.Decompress);
+            reader = new StreamReader(gzStream);
+        }
+
+        public string? ReadLine()
+        {
+            start:
+            if (reader.ReadLine() is string line)
+            {
+                var doc = JsonDocument.Parse(line);
+                if(doc.RootElement.TryGetProperty("text", out var text) && text.GetString() is string textString)
+                {
+                    LinesRead++;
+                    return textString;
+                }
+                goto start;
+            }
+            return null;
+        }
+
+        public void Dispose()
+        {
+            reader.Dispose();
+            gzStream.Dispose();
+            stream.Dispose();
+        }
+    }
+}
--- MachineLearning.Samples/Language/SLM3.cs
+++ MachineLearning.Samples/Language/SLM3.cs
@@ -1,84 +1,88 @@
-//using MachineLearning.Model.Layer;
+using MachineLearning.Data;
+using MachineLearning.Model.Layer;
 
-//namespace MachineLearning.Samples.Language;
+namespace MachineLearning.Samples.Language;
 
-//public sealed class SLM3
-//{
-//    public const string TOKENS = " %'(),-.0123456789:=abcdefghijklmnopqrstuvwxyz\0";
-//    public const int CONTEXT_SIZE = 128;
+public sealed class SLM3
+{
+    // Console.WriteLine(string.Join(", ",
+    //     dataSet.GetLines().Take(20000)
+    //     .SelectMany(l => l.Split([' ', '.', ',', '!', '?', ';', ':'], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+    //     .Select(s => s.Replace('’', '\''))
+    //     .CountBy(w => w, StringComparer.InvariantCultureIgnoreCase).OrderByDescending(g => g.Value).Select(g => g.Key).Where(s => s.Length > 1).Select(s => $"\"{s.ToLower()}\"").Take(255-SLM3.SYMBOLS.Length)
+    // ));
+    public static readonly HashSet<string> WORD_TOKENS = ["the", "and", "to", "of", "in", "is", "for", "that", "you", "with", "on", "it", "are", "as", "this", "be", "your", "or", "have", "at", "was", "from", "we", "by", "will", "not", "can", "an", "but", "all", "they", "if", "has", "our", "my", "more", "their", "one", "so", "he", "about", "which", "when", "what", "also", "out", "his", "up", "there", "time", "new", "do", "who", "like", "some", "other", "been", "just", "get", "how", "her", "would", "had", "them", "were", "any", "no", "these", "into", "me", "than", "people", "its", "make", "most", "only", "may", "she", "us", "first", "over", "use", "work", "very", "after", "well", "then", "now", "many", "need", "even", "through", "way", "two", "good", "best", "because", "see", "years", "know", "where", "day", "should", "much", "could", "such", "great", "here", "while", "take", "help", "home", "said", "back", "want", "it's", "being", "before", "year", "those", "find", "each", "made", "right", "used", "life", "go", "world", "free", "information", "business", "really", "every", "love", "think", "own", "both", "still", "around", "him", "last", "going", "off", "same", "different", "look", "place", "part", "between", "too", "did", "down", "service", "am", "during", "does", "since", "using", "high", "things", "company", "always", "another", "few", "set", "little", "available", "long", "services", "without", "online", "don't", "system", "family", "experience", "something", "come", "data", "next", "school", "why", "better", "sure", "under", "give", "however", "must", "including", "support", "can't"];
+    public const string SYMBOLS = "\0 ?!\"#$%&'()*+,-./0123456789:;=?_abcdefghijklmnopqrstuvwxyz|ßäöü€";
+    public const int CONTEXT_SIZE = 128 + 64;
+    public static StringTokenizer Tokenizer { get; } = new(WORD_TOKENS, SYMBOLS, [("“", "\""), ("”", "\""), ("–", "-"), ("—", "-"), ("’", "'"), ("it’s", "it's"), ("don’t", "don't"), ("can’t", "can't")]);
+    public static ModelSerializer Serializer { get; } = new(AssetManager.GetModelFile("slm3.gmw"));
+    public static EmbeddedModel<int[], int> CreateModel(Random? random = null)
+    {
+        var initializer = new HeInitializer(random);
+        return AdvancedModelBuilder
+            .Create(new EncodedEmbeddingLayer(Tokenizer.TokenCount, CONTEXT_SIZE))
+                .DefaultActivation(LeakyReLUActivation.Instance)
+                .AddLayer(1024 * 2, initializer)
+                .AddLayer(1024 * 2 , initializer)
+                .AddLayer(1024, initializer)
+                .AddLayer(1024, initializer)
+                .AddLayer(Tokenizer.TokenCount, new XavierInitializer(random), SoftMaxActivation.Instance)
+            .AddOutputLayer(new TokenOutputLayer(Tokenizer.TokenCount, true, random));
+    }
 
-//    public static IOutputResolver<char> OutputResolver { get; } = new CharOutputResolver(TOKENS);
-//    public static ModelSerializer Serializer { get; } = new(AssetManager.GetModelFile("sentences_3.gmw"));
-//    public static FeedForwardModel<string, char> CreateModel(Random? random = null)
-//    {
-//        var initializer = new HeInitializer(random);
-//        return AdvancedModelBuilder
-//            .Create<StringEmbeddingLayer, string>(new StringEmbeddingLayer(TOKENS, CONTEXT_SIZE, 12), new StringEmbeddingLayer.Initializer(random))
-//                .SetDefaultActivationFunction(LeakyReLUActivation.Instance)
-//                .AddLayer(1024 * 2, initializer)
-//                .AddLayer(1024 * 2, initializer)
-//                .AddLayer(512 + 256, initializer)
-//                .AddLayer(512, initializer)
-//                .AddLayer(TOKENS.Length, new XavierInitializer(random), SoftMaxActivation.Instance)
-//            .AddOutputLayer(new TokenOutputLayer(TOKENS, true, random));
-//    }
+    public static EmbeddedModel<int[], int> TrainDefault(EmbeddedModel<int[], int>? model = null, TrainingConfig? config = null, ITrainingSet? trainingSet = null, Random? random = null)
+    {
+        model ??= Serializer.Load<EmbeddedModel<int[], int>>().Or(error =>
+        {
+            Console.WriteLine("No existing model found! Creating new!");
+            return CreateModel(random);
+        });
 
-//    public static IEmbeddedModel<string, char> TrainDefault(IEmbeddedModel<string, char>? model = null, TrainingConfig<string, char>? config = null, Random? random = null)
-//    {
-//        model ??= CreateModel(random);
+        var trainer = ModelTrainer.Create(model, config ?? DefaultTrainingConfig(), trainingSet ?? GetTrainingSet());
+        trainer.TrainConsole();
+        Serializer.Save(model).Consume(
+            () => Console.WriteLine("Model saved!"),
+            error => Console.WriteLine($"Error saving model: {error.Message}")
+        );
+        LMHelper.StartChat(model, CONTEXT_SIZE, Tokenizer);
+        return model;
+    }
 
-//        var trainer = ModelTrainer.Generic(model, config ?? DefaultTrainingConfig());
-//        trainer.TrainConsole();
-//        Serializer.Save(model).Consume(
-//            () => Console.WriteLine("Model saved!"),
-//            flag => Console.WriteLine($"Error saving model: {flag}")
-//        );
-//        LMHelper.StartChat(model, CONTEXT_SIZE);
-//        return model;
-//    }
+    public static TrainingConfig DefaultTrainingConfig(Random? random = null) => new()
+    {
+        EpochCount = 32,
 
-//    public static TrainingConfig<string, char> DefaultTrainingConfig(Random? random = null)
-//    {
-//        random ??= Random.Shared;
+        Optimizer = new AdamOptimizer
+        {
+            LearningRate = 0.01f,
+            CostFunction = CrossEntropyLoss.Instance,
+        },
 
-//        var dataSet = GetTrainingSet().ToArray();
-//        random.Shuffle(dataSet);
+        EvaluationCallback = result => Console.WriteLine(result.Dump()),
+        DumpEvaluationAfterBatches = 16,
+        //MultiThread = false,
+        RandomSource = random ?? Random.Shared,
+    };
 
-//        var trainingSetSize = (int)(dataSet.Length * 0.9);
+    public static ITrainingSet GetTrainingSet(Random? random = null)
+    {
+        Console.WriteLine("Analyzing Trainings Data...");
+        var lines = LanguageDataHelper.GetLines(AssetManager.Sentences).ToArray();
+        Console.WriteLine($"Longest sentence {lines.Max(s => s.Length)} chars");
+        var tokensUsedBySource = new string([.. lines.SelectMany(s => s).Distinct().Order()]);
+        Console.WriteLine($"Source uses '{tokensUsedBySource}'");
 
-//        return new TrainingConfig<string, char>()
-//        {
-//            TrainingSet = dataSet,
-//            TestSet = dataSet.Skip(trainingSetSize).ToArray(),
+        Console.WriteLine(lines.SelectDuplicates().Dump('\n'));
 
-//            EpochCount = 32,
-//            BatchCount = 256,
+        //var words = lines.SelectMany(l => l.Split([' ', '.', ','], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries));
+        //var usages = words.CountBy(w => w).OrderByDescending(g => g.Value).Select(g => $"{g.Key}: {g.Value}");
+        //Console.WriteLine(string.Join('\n', usages.Take(50)));
+        var endToken = Tokenizer.TokenizeSingle("\0");
 
-//            Optimizer = new AdamOptimizer
-//            {
-//                LearningRate = 0.01,
-//                CostFunction = CrossEntropyLoss.Instance,
-//            },
-
-//            OutputResolver = OutputResolver,
-
-//            EvaluationCallback = result => Console.WriteLine(result.Dump()),
-//            DumpEvaluationAfterBatches = 1,
-
-//            RandomSource = random,
-//        };
-//    }
-
-//    public static IEnumerable<DataEntry<string, char>> GetTrainingSet(Random? random = null)
-//    {
-//        Console.WriteLine("Analyzing Trainings Data...");
-//        var lines = LanguageDataSource.GetLines(AssetManager.Sentences).ToArray();
-//        Console.WriteLine($"Longest sentence {lines.Max(s => s.Length)} chars");
-//        var tokensUsedBySource = new string([.. lines.SelectMany(s => s).Distinct().Order()]);
-//        Console.WriteLine($"Source uses '{tokensUsedBySource}'");
-//        tokensUsedBySource.Consume(t => OutputResolver.Expected(t));
-
-//        Console.WriteLine(lines.SelectDuplicates().Dump('\n'));
-//        return lines.InContextSize(CONTEXT_SIZE).ExpandPerChar();
-//    }
-//}
+        return new PredefinedTrainingSet(lines.Tokenize(Tokenizer).ExpandPerToken(endToken, CONTEXT_SIZE).ToTrainingData(Tokenizer.TokenCount)) 
+        {
+            BatchCount = 256,
+            Random = random ?? Random.Shared,
+        };
+    }
+}
